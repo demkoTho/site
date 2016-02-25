aeb1aba61dc3654e0ec3fd75bf2abd72fb557d77
Foenki <pierrot.lagier@wanadoo.fr>
2016-02-25 15:23:26 +0100

    Ajout page de draft d'arène
    - Création de la page
    - Ecran de sélection de classe
    - Changements dans l'organisation des images

---
+++ js/arenaDraft.js
@@ -0,0 +1,43 @@
+var nbCards = 0;
+var cadre;
+var cardsInDeck;
+var classes = ['shaman', 'paladin', 'priest', 'rogue', 'mage', 'warrior', 'druid', 'warlock', 'hunter'];
+var choosenClass;
+
+function activate(hero)
+{
+	if(choosenClass == null)
+	{
+		$("#button_init").removeClass("disabled");
+		$("#button_init").text("Commencer la draft");
+		
+	}
+	choosenClass = hero;
+	for(var i = 0; i < classes.length; i++)
+	{
+		var c = classes[i];
+		if(c == hero)
+		{
+			$("."+c).css("opacity", "0.95");
+		}
+		else
+		{
+			$("."+c).css("opacity", "0.7");
+		}
+	}
+}
+
+function init_draft()
+{
+	cadre = $("#draftDiv");
+	if(choosenClass != null)
+	{
+		cadre.html('<p>Deck : ' + nbCards + ' cartes</p><div class="col-xs-3" id="deck_status"></div><div class="col-xs-9" id="card_choice"></div>');
+		displayNewCards();
+	}
+}
+
+function displayNewCards()
+{
+	var zone = $("#card_choice");
+}
