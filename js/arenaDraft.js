2622a008336c6ee9940296a9f8ee1999e84b2085
Foenki <pierrot.lagier@wanadoo.fr>
2016-02-25 17:50:32 +0100

    Sélection des cartes par rareté

--- js/arenaDraft.js
+++ js/arenaDraft.js
@@ -1,20 +1,38 @@
 var nbCards = 0;
 var cadre;
 var cards;
+var rarity;
 var cardsInDeck;
 var classes = ['shaman', 'paladin', 'priest', 'rogue', 'mage', 'warrior', 'druid', 'warlock', 'hunter'];
 var choosenClass;
+var probas = [0.6,0.3,0.08,0.02];
+
+function init_draft()
+{
+	if(choosenClass != null)
+	{
+		loadCards();
+	}
+}
 
 function loadCards() {
 	
 	$.ajax({
 		dataType: "json",
-		url: "http://api.hearthstonejson.com/v1/latest/enUS/cards.json",
+		url: "http://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json",
 		data: "",
 		success: function(json) {
-			console.log(json); // this will show the info it in firebug console
+			cards = json.filter(filterClassCard);
+			cadre = $("#draftDiv");
+			cadre.html('<p>Deck : ' + nbCards + ' cartes</p><div class="col-xs-3" id="deck_status"></div><div class="col-xs-9" id="card_choice"></div>');
+			displayNewCards();
 		}
 	});
+}
+
+function filterClassCard(obj) 
+{
+	return ! ('playerClass' in obj && obj.playerClass != choosenClass.toUpperCase())
 }
       
 function activate(hero)
@@ -40,17 +58,46 @@ function activate(hero)
 	}
 }
 
-function init_draft()
-{
-	cadre = $("#draftDiv");
-	if(choosenClass != null)
-	{
-		cadre.html('<p>Deck : ' + nbCards + ' cartes</p><div class="col-xs-3" id="deck_status"></div><div class="col-xs-9" id="card_choice"></div>');
-		displayNewCards();
-	}
-}
-
 function displayNewCards()
 {
 	var zone = $("#card_choice");
+	rarity = getRarity(Math.random())
+	console.log(rarity);
+	
+	var pool = cards.filter(filterCardRarity);
+	
+	var chosenCards = [];
+	for(var i = 0; i < 3; i++)
+	{
+		chosenCards.push(pool[Math.floor(Math.random() * pool.length)]);
+		zone.append($("<p>").text(chosenCards[i].name));
+	}
+
+	console.log(chosenCards);
+	
+}
+
+function filterCardRarity(obj)
+{
+	return (obj.rarity == rarity || (rarity == 'COMMON' && obj.rarity =='FREE'))
+}
+
+function getRarity(rand)
+{
+	if(rand <= probas[0])
+	{
+		return 'COMMON';
+	}
+	else if(rand <= probas[0] + probas[1])
+	{
+		return 'RARE';
+	}
+	else if(rand <= probas[0] + probas[1] + probas[2])
+	{
+		return 'EPIC';
+	}
+	else
+	{
+		return 'LEGENDARY';
+	}
 }
