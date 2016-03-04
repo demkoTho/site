ef44cfee9bf06839723acf48c5ee593b60f4648d
Foenki <pierrot.lagier@wanadoo.fr>
2016-03-04 13:13:58 +0100

    Changement affichage decks
    Finalisation des decks freeze et controle

--- js/decks.js
+++ js/decks.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 	$("td.card").mouseover(function(){
 
 		var name = $(this).text();
-		var s = "<img src='images/cartes/"+name+".png' alt='"+name+"'>";
+		var s = '<img src="images/cartes/'+name+'.png" alt="'+name+'">';
 		$(".preview").html(s);
 		console.log(name);
 	});
